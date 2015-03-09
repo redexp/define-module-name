6b354d355b874fd457cbd95dc2500a260411c2dd
Sergii.Kliuchnyk <sergii.kliuchnyk@masterofcode.com>
2015-03-09 08:38:24 +0200

    v1

---
+++ define-module-name.js
@@ -0,0 +1,139 @@
+;(function (root) {
+
+    root.define = define;
+    root.require = require;
+
+    define.amd = {
+        jQuery: true
+    };
+
+    define.clear = clear;
+
+    require.config = function () {};
+
+    var modules, names;
+
+    clear();
+
+    function define(name, deps, cb) {
+        if (arguments.length === 1) {
+            switch (typeof name) {
+                case 'string':
+                    nextName(name);
+                    return;
+
+                case 'function':
+                    cb = name;
+                    name = nextName();
+                    deps = ['require', 'exports', 'module'];
+                    break;
+
+                case 'object':
+                    var result = name;
+                    name = nextName();
+                    deps = [];
+                    cb = function () {
+                        return result;
+                    };
+                    break;
+
+                default:
+                    throw new Error('Unknown one argument type: ' + typeof name);
+            }
+        }
+        else if (arguments.length === 2) {
+            cb = deps;
+            deps = name;
+            name = nextName();
+        }
+        else if (names.length > 0) {
+            name = nextName();
+        }
+
+        if (typeof name !== 'string') {
+            throw new Error('Invalid module name type: ' + typeof name);
+        }
+
+        modules[name] = {
+            callback: cb,
+            deps: deps,
+            called: false,
+            pending: false,
+            result: null
+        };
+    }
+
+    function require(deps, cb) {
+        if (arguments.length === 1) {
+            switch (typeof deps) {
+                case 'string':
+                    return moduleResult(deps);
+
+                case 'object':
+                    cb = function () {};
+                    break;
+            }
+        }
+
+        cb.apply(null, deps.map(moduleResult));
+    }
+
+    function moduleResult(name) {
+        if (typeof name !== 'string') return name;
+
+        switch (name) {
+            case 'require':
+                return require;
+        }
+
+        if (!has(modules, name)) {
+            throw new Error('Undefined module: ' + name);
+        }
+
+        var module = modules[name];
+
+        if (!module.called) {
+            if (module.pending) return;
+
+            module.pending = true;
+
+            var moduleExports = {exports: {}},
+                hasModuleExports = false;
+
+            var exportsIndex = module.deps.indexOf('exports');
+            if (exportsIndex > -1) {
+                hasModuleExports = true;
+                module.deps.splice(exportsIndex, 1, moduleExports.exports);
+            }
+
+            var moduleIndex = module.deps.indexOf('module');
+            if (moduleIndex > -1) {
+                hasModuleExports = true;
+                module.deps.splice(moduleIndex, 1, moduleExports);
+            }
+
+            var result = module.callback.apply(null, module.deps.map(moduleResult));
+
+            module.result = hasModuleExports ? moduleExports.exports : result;
+
+            module.called = true;
+            module.pending = false;
+        }
+
+        return modules[name].result;
+    }
+
+    function nextName(name) {
+        return name ? names.push(name) : names.shift();
+    }
+
+    function clear() {
+        modules = define.modules = {};
+        names = define.names = [];
+    }
+
+    function has(obj, field) {
+        return Object.prototype.hasOwnProperty.call(obj, field);
+    }
+
+}(typeof exports === 'object' ? exports : this));
---
+++ test/index.js
@@ -0,0 +1,167 @@
+var d = require('../define-module-name');
+var expect = require('chai').expect;
+
+describe('define', function () {
+
+    beforeEach(function () {
+        d.define.clear();
+    });
+
+    it('should define a module', function () {
+        var calls = 0;
+        d.define('test', [], function () {
+            calls++;
+            return 1;
+        });
+
+        var num = 0;
+
+        d.require(['test'], function (test) {
+            num++;
+            expect(test).to.equal(1);
+        });
+
+        d.require(['test']);
+
+        expect(d.require('test')).to.equal(1);
+
+        expect(calls).to.equal(1);
+        expect(num).to.equal(1);
+    });
+
+    it('should return require, exports and module', function () {
+        d.define('x', [], function () {
+            return 1;
+        });
+
+        var num = 0;
+
+        d.define('test', ['module', 'exports', 'require', 'x'], function (m, e, req, x) {
+            num++;
+
+            expect(m.exports).to.equal(e);
+            expect(req).to.equal(d.require);
+            expect(x).to.equal(1);
+            expect(req('x')).to.equal(1);
+
+            e.test = 1;
+
+            return 2;
+        });
+
+        expect(d.require('test')).to.deep.equal({test: 1});
+    });
+
+    it('should handle recursion', function () {
+        var num = 0;
+
+        d.define('x', ['test'], function (test) {
+            num++;
+
+            expect(test).to.equal(3);
+            return 1;
+        });
+
+        d.define('test', ['module', 'x'], function (m, x) {
+            num++;
+
+            expect(x).to.be.undefined;
+
+            m.exports = 3;
+
+            return 2;
+        });
+
+        expect(d.require('x')).to.equal(1);
+        expect(num).to.equal(2);
+    });
+
+    it('should handle name', function () {
+        var num = 0;
+
+        d.define('test');
+
+        d.define([], function () {
+            num++;
+            return 1;
+        });
+
+        d.define('x');
+
+        d.define([], function () {
+            num++;
+            return 2;
+        });
+
+        d.define('y');
+        d.define('z');
+
+        d.define(['x'], function (x) {
+            num++;
+
+            expect(x).to.equal(2);
+
+            return 3;
+        });
+
+        d.define(['test', 'y'], function (t, y) {
+            num++;
+
+            expect(t).to.equal(1);
+            expect(y).to.equal(3);
+
+            return 4;
+        });
+
+        expect(d.require('test')).to.equal(1);
+        expect(d.require('x')).to.equal(2);
+        expect(d.require('y')).to.equal(3);
+        expect(d.require('z')).to.equal(4);
+
+        expect(num).to.equal(4);
+    });
+
+    it('should overwrite name', function () {
+        var num = 0;
+
+        d.define('test');
+
+        d.define('x', [], function () {
+            num++;
+            return 1;
+        });
+
+        expect(d.require('test')).to.equal(1);
+
+        try {
+            d.require('x');
+        }
+        catch(e) {
+            num++;
+            expect(e.message).to.equal('Undefined module: x');
+        }
+
+        expect(num).to.equal(2);
+    });
+
+    it('should handle name and function only', function () {
+        var num = 0;
+
+        d.define('test');
+
+        d.define(function (req, e, m) {
+            num++;
+
+            expect(req).to.equal(d.require);
+            expect(e).to.be.object;
+            expect(e).to.equal(m.exports);
+
+            m.exports = 1;
+        });
+
+        expect(d.require('test')).to.equal(1);
+        expect(d.require('test')).to.equal(1);
+        expect(num).to.equal(1);
+    });
+
+});
